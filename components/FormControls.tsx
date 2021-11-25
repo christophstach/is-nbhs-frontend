62cabaf76013a189ec608caa2bac0f91231bc812
Christoph Stach <christoph.stach@gmail.com>
2021-11-25 14:00:24 +0100

    WIP

---
+++ components/FormControls.tsx
@@ -0,0 +1,253 @@
+import * as React from 'react';
+import { FunctionComponent, useState } from 'react';
+import TableRow from '@mui/material/TableRow';
+import { TableCell, TextField } from '@mui/material';
+import Button from '@mui/material/Button';
+import { Controller, useFormContext } from "react-hook-form";
+
+export interface AcquisitionTimeSpanProps {
+
+}
+
+export interface SubFormProps {
+    name: string;
+}
+
+export const SubForm: FunctionComponent<SubFormProps> = (props) => {
+    return (
+        <TableRow>
+            <TableCell colSpan={9} sx={{backgroundColor: '#EEEEEE'}}>
+                <h3>{props.name}</h3>
+            </TableCell>
+        </TableRow>
+    );
+}
+
+export interface WeekAcquisitionItem {
+    name: string;
+    nameEditable: boolean;
+    values: [number, number, number, number, number, number, number];
+}
+
+export interface WeekAcquisitionMultipleProps {
+    extendable: boolean;
+    name: string;
+    items: WeekAcquisitionItem[];
+}
+
+export const WeekAcquisitionMultiple: FunctionComponent<WeekAcquisitionMultipleProps> = (props) => {
+    const [items, setItems] = useState<WeekAcquisitionItem[]>(props.items);
+    const {control, watch} = useFormContext();
+    const watchFields = watch();
+
+
+    function handleAddItem() {
+        setItems([
+            ...items,
+            {
+                name: '',
+                nameEditable: true,
+                values: [0, 0, 0, 0, 0, 0, 0]
+            }
+        ])
+    }
+
+
+    return (
+        <>
+            {items.map((item, index) => {
+                return (
+                    <TableRow key={index}>
+                        <TableCell sx={{width: '100%'}}>
+                            <Controller name={`${props.name}.${index}.name`}
+                                        control={control}
+                                        defaultValue={item.name}
+                                        render={({field}) => (
+                                            <TextField {...field}
+                                                       label="Name"
+                                                       variant="filled"
+                                                       size="small"
+                                                       sx={{width: '100%'}}/>
+                                        )}/>
+
+                        </TableCell>
+                        <TableCell>
+                            <Controller name={`${props.name}.${index}.monday`}
+                                        control={control}
+                                        defaultValue={0}
+                                        render={({field}) => (
+                                            <TextField {...field}
+                                                       label="Mo"
+                                                       variant="filled"
+                                                       size="small"
+                                                       onChange={(e) => {
+                                                           field.onChange(parseInt(e.target.value), 10);
+                                                       }}
+                                                       InputProps={{inputProps: {min: 0, step: 1}}}
+                                                       type="number"
+                                                       sx={{width: '80px'}}/>
+                                        )}/>
+                        </TableCell>
+                        <TableCell>
+                            <Controller name={`${props.name}.${index}.tuesday`}
+                                        control={control}
+                                        defaultValue={0}
+                                        render={({field}) => (
+                                            <TextField {...field}
+                                                       label="Di"
+                                                       variant="filled"
+                                                       size="small"
+                                                       onChange={(e) => field.onChange(parseInt(e.target.value), 10)}
+                                                       InputProps={{inputProps: {min: 0, step: 1}}}
+                                                       type="number"
+                                                       sx={{width: '80px'}}/>
+                                        )}/>
+                        </TableCell>
+                        <TableCell>
+                            <Controller name={`${props.name}.${index}.wednesday`}
+                                        control={control}
+                                        defaultValue={0}
+                                        render={({field}) => (
+                                            <TextField {...field}
+                                                       label="Mi"
+                                                       variant="filled"
+                                                       size="small"
+                                                       onChange={(e) => field.onChange(parseInt(e.target.value), 10)}
+                                                       InputProps={{inputProps: {min: 0, step: 1}}}
+                                                       type="number"
+                                                       sx={{width: '80px'}}/>
+                                        )}/>
+                        </TableCell>
+                        <TableCell>
+                            <Controller name={`${props.name}.${index}.thursday`}
+                                        control={control}
+                                        defaultValue={0}
+                                        render={({field}) => (
+                                            <TextField {...field}
+                                                       label="Do"
+                                                       variant="filled"
+                                                       size="small"
+                                                       onChange={(e) => field.onChange(parseInt(e.target.value), 10)}
+                                                       InputProps={{inputProps: {min: 0, step: 1}}}
+                                                       type="number"
+                                                       sx={{width: '80px'}}/>
+                                        )}/>
+                        </TableCell>
+                        <TableCell>
+                            <Controller name={`${props.name}.${index}.friday`}
+                                        control={control}
+                                        defaultValue={0}
+                                        render={({field}) => (
+                                            <TextField {...field}
+                                                       label="fr"
+                                                       variant="filled"
+                                                       size="small"
+                                                       onChange={(e) => field.onChange(parseInt(e.target.value), 10)}
+                                                       InputProps={{inputProps: {min: 0, step: 1}}}
+                                                       type="number"
+                                                       sx={{width: '80px'}}/>
+                                        )}/>
+                        </TableCell>
+                        <TableCell>
+                            <Controller name={`${props.name}.${index}.saturday`}
+                                        control={control}
+                                        defaultValue={0}
+                                        render={({field}) => (
+                                            <TextField {...field}
+                                                       label="Sa"
+                                                       variant="filled"
+                                                       size="small"
+                                                       onChange={(e) => field.onChange(parseInt(e.target.value), 10)}
+                                                       InputProps={{inputProps: {min: 0, step: 1}}}
+                                                       type="number"
+                                                       sx={{width: '80px'}}/>
+                                        )}/>
+                        </TableCell>
+                        <TableCell>
+                            <Controller name={`${props.name}.${index}.sunday`}
+                                        control={control}
+                                        defaultValue={0}
+                                        render={({field}) => (
+                                            <TextField {...field}
+                                                       label="So"
+                                                       variant="filled"
+                                                       size="small"
+                                                       onChange={(e) => field.onChange(parseInt(e.target.value), 10)}
+                                                       InputProps={{inputProps: {min: 0, step: 1}}}
+                                                       type="number"
+                                                       sx={{width: '80px'}}/>
+                                        )}/>
+                        </TableCell>
+                        <TableCell
+                            sx={{verticalAlign: 'bottom'}}>
+                            <strong style={{
+                                display: 'block',
+                                fontSize: '16px',
+                                width: '80px',
+                                padding: '21px 12px 3px 12px',
+                                backgroundColor: '#EEEEEE',
+                                textAlign: 'center',
+                                borderRadius: '4px 4px 0 0',
+                                borderBottom: '1px solid rgba(0, 0, 0, 0.42)'
+                            }}>
+                                {
+                                    watchFields[props.name] ? (
+                                        watchFields[props.name][index].monday +
+                                        watchFields[props.name][index].tuesday +
+                                        watchFields[props.name][index].wednesday +
+                                        watchFields[props.name][index].thursday +
+                                        watchFields[props.name][index].friday +
+                                        watchFields[props.name][index].saturday +
+                                        watchFields[props.name][index].sunday
+                                    ) : (<></>)
+                                }
+                            </strong>
+                        </TableCell>
+                    </TableRow>
+                );
+            })}
+            {props.extendable ? (
+                <TableRow>
+                    <TableCell colSpan={9}>
+                        <Button color="primary" variant="contained" onClick={handleAddItem}>Eintrag hinzufügen</Button>
+                    </TableCell>
+                </TableRow>
+            ) : (
+                <></>
+            )}
+        </>
+    );
+}
+
+
+export interface OneTimeAcquisitionProps {
+
+}
+
+
+export interface CommentProps {
+    name: string;
+    label: string;
+}
+
+export const Comment: FunctionComponent<CommentProps> = (props) => {
+    const {control} = useFormContext();
+
+    return (
+        <>
+            <TableRow>
+                <TableCell colSpan={9}>
+                    <Controller name={props.name} control={control} defaultValue="" render={({field}) => (
+                        <TextField {...field}
+                                   label={props.label}
+                                   variant="filled"
+                                   size="small"
+                                   rows={6}
+                                   multiline
+                                   fullWidth/>
+                    )}/>
+                </TableCell>
+            </TableRow>
+        </>
+    )
+}
--- pages/forms/gtb-os/[id]/index.tsx
+++ pages/forms/gtb-os/[id]/index.tsx
@@ -1,16 +1,90 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
 import * as React from 'react';
-import { useRouter } from 'next/router'
-import Paper from '@mui/material/Paper';
-import { TextField } from '@mui/material';
-import Box from '@mui/material/Box';
+import { useRouter } from 'next/router';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import { Comment, SubForm, WeekAcquisitionMultiple } from '../../../../components/FormControls';
+import { Card, CardActions } from '@mui/material';
+import Button from '@mui/material/Button';
+import { SubmitHandler, useForm, FormProvider } from 'react-hook-form';
+
+const tableStructure = {
+    elements: [
+        {
+            type: 'subForm',
+            name: 'Beratungen (SuS, LuL, Eltern)'
+        },
+        {
+            type: 'weekAcquisitionMultiple',
+            name: 'consulting',
+            items: [
+                {name: 'Kurzberatung', nameEditable: false, values: [0, 0, 0, 0, 0, 0, 0]},
+                {name: 'Beratungen > 15 min', nameEditable: false, values: [0, 0, 0, 0, 0, 0, 0]},
+                {name: 'zu beratende Personen', nameEditable: false, values: [0, 0, 0, 0, 0, 0, 0]}
+            ],
+            extendable: false
+        },
+        {
+            type: 'subForm',
+            name: 'Beratungsthemen',
+        },
+        {
+            type: 'weekAcquisitionMultiple',
+            name: 'consultingTopics',
+            items: [
+                {name: 'psychosoziale Beratung', nameEditable: false, values: [0, 0, 0, 0, 0, 0, 0]},
+                {name: 'Konflikt', nameEditable: false, values: [0, 0, 0, 0, 0, 0, 0]}
+            ],
+            extendable: true
+        },
+        {
+            type: 'subForm',
+            name: 'Gruppen und Kurse (sozialpädagogische Gruppenangebote, Klassenangebote, Einzelfallhilfe u.ä.)'
+        },
+        {
+            type: 'subForm',
+            name: 'Besuchende bei sonstigen Aktivitäten (z.B. Pausenangebote, offene Angebote)'
+        },
+        {
+            type: 'subForm',
+            name: 'Ehrenamtliche Mitarbeitende'
+        },
+        {
+            type: 'subForm',
+            name: 'Kontaktaufnahme mit externen Hifsangeboten, Jugendamt, Beratungsstellen etc.'
+        },
+
+        {
+            type: 'subForm',
+            name: 'Gremien, Vernetzung und Kooperation (innerhalb und außerhalb der Schule)'
+        },
+
+        {
+            type: 'subForm',
+            name: 'Anzahl der Schülerinnen und Schüler an der Schule'
+        },
+        {
+            type: 'subForm',
+            name: 'Bemerkungen'
+        },
+        {
+            type: 'comment',
+            name: 'comment',
+            label: 'Bemerkung'
+        }
+    ]
+}
 
 const GtbOsSingleIndex: NextPage = () => {
     const router = useRouter();
     const {id} = router.query;
+    const methods = useForm();
 
-    console.log(router.query);
+    const formSubmitHandler: SubmitHandler<any> = (data) => {
+        console.log('form data', data);
+    };
 
     return (
         <>
@@ -18,19 +92,43 @@ const GtbOsSingleIndex: NextPage = () => {
                 <title>NBHS: Formulare - GTB OS - {id}</title>
             </Head>
 
-            <Paper sx={{padding: '1rem'}}>
-                <Box sx={{display: 'flex', gap: '1rem'}}>
+            <FormProvider {...methods}>
+                <form onSubmit={methods.handleSubmit(formSubmitHandler)}>
+                    <Card>
+                        <CardActions>
+                            <Button type="submit" color="success" variant="contained">Speichern</Button>
+                        </CardActions>
 
-                    <TextField label="Mo" variant="filled" size="small"/>
-                    <TextField label="Di" variant="filled" size="small"/>
-                    <TextField label="Mi" variant="filled" size="small"/>
-                    <TextField label="Do" variant="filled" size="small"/>
-                    <TextField label="Fr" variant="filled" size="small"/>
-                    <TextField label="Sa" variant="filled" size="small"/>
-                    <TextField label="So" variant="filled" size="small"/>
-                    <TextField label="Gesamt" variant="filled" size="small"/>
-                </Box>
-            </Paper>
+                        <TableContainer>
+                            <Table size="small">
+                                <TableBody>
+                                    {tableStructure.elements.map((element, indexElement) => {
+                                        switch (element.type) {
+                                            case 'subForm':
+                                                return (
+                                                    <SubForm key={indexElement} name={element.name as string}/>
+                                                );
+                                            case 'comment':
+                                                return (
+                                                    <Comment key={indexElement}
+                                                             name={element.name as string}
+                                                             label={element.label as string}/>
+                                                )
+                                            case 'weekAcquisitionMultiple':
+                                                return (
+                                                    <WeekAcquisitionMultiple key={indexElement}
+                                                                             name={element.name as string}
+                                                                             extendable={element.extendable as boolean}
+                                                                             items={element.items as any}/>
+                                                )
+                                        }
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Card>
+                </form>
+            </FormProvider>
         </>
     )
 }
