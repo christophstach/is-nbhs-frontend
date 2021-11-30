ebde63edf6237a8571e210e6bbc331e86ed495ad
Christoph Stach <christoph.stach@gmail.com>
2021-11-30 15:24:15 +0100

    Add name to interface

--- pages/areas/areas-manage/index.tsx
+++ pages/areas/areas-manage/index.tsx
@@ -18,12 +18,13 @@ import Button from '@mui/material/Button';
 import { useAreasManage } from '../../../hooks/clients/use-areas-manage';
 
 export interface AreasManageResponse {
-        data: {
+    data: {
         id: number,
+        name: string,
         areatype: string,
         comment: string
-        }[],
-        meta: {}
+    }[],
+    meta: {}
 }
 
 const AreasManageIndex: NextPage = () => {
@@ -31,52 +32,52 @@ const AreasManageIndex: NextPage = () => {
     const areas = response.data?.data;
 
     return (
-    <>
-        <Head>
-            <title>NBHS: Bereichsverwaltung</title>
-        </Head>
+        <>
+            <Head>
+                <title>NBHS: Bereichsverwaltung</title>
+            </Head>
 
-        <TopNavPortal>
-            <Button variant="contained">Neuen Bereich anlegen</Button>
-        </TopNavPortal>
+            <TopNavPortal>
+                <Button variant="contained">Neuen Bereich anlegen</Button>
+            </TopNavPortal>
 
-        <TableContainer component={Paper}>
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>Id</TableCell>
-                    <TableCell>Bereichsname</TableCell>
-                    <TableCell>Bereichstyp</TableCell>
-                    <TableCell>Beschreibung</TableCell>
-                </TableRow>
-            </TableHead>
-        <TableBody>
-            {areas ? (
-            areas.map((area) => (
-            <TableRow key={area.id}>
-            <TableCell>{area.id}</TableCell>
-            <TableCell>{area.name}</TableCell>
-            <TableCell>{area.areatype}</TableCell>
-            <TableCell>{area.comment}</TableCell>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Id</TableCell>
+                            <TableCell>Bereichsname</TableCell>
+                            <TableCell>Bereichstyp</TableCell>
+                            <TableCell>Beschreibung</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {areas ? (
+                            areas.map((area) => (
+                                <TableRow key={area.id}>
+                                    <TableCell>{area.id}</TableCell>
+                                    <TableCell>{area.name}</TableCell>
+                                    <TableCell>{area.areatype}</TableCell>
+                                    <TableCell>{area.comment}</TableCell>
 
-            <TableCell sx={{whiteSpace: 'nowrap'}}>
-            <IconButton component="a" href={`/areas/areas-manage/${area.id}`}>
-            <EditIcon color="warning"/></IconButton>
-            <IconButton component="a" href={`/areas/areas-manage/${area.id}/delete`}>
-            <DeleteIcon color="error"/></IconButton>
-            <IconButton component="a" href={`/areas/areas-manage/${area.id}/review`}>
-            <InfoIcon color="info"/></IconButton>
-             </TableCell>
-            </TableRow>
-                ))
-        ) : (
-        <TableRow>
-        <TableCell colSpan={6} sx={{textAlign: 'center'}}>Loading...</TableCell>
-        </TableRow>
-        )}
-        </TableBody>
-        </Table>
-        </TableContainer>
+                                    <TableCell sx={{whiteSpace: 'nowrap'}}>
+                                        <IconButton component="a" href={`/areas/areas-manage/${area.id}`}>
+                                            <EditIcon color="warning"/></IconButton>
+                                        <IconButton component="a" href={`/areas/areas-manage/${area.id}/delete`}>
+                                            <DeleteIcon color="error"/></IconButton>
+                                        <IconButton component="a" href={`/areas/areas-manage/${area.id}/review`}>
+                                            <InfoIcon color="info"/></IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={6} sx={{textAlign: 'center'}}>Loading...</TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     )
 }
